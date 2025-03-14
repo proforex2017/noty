60c57a2aa26c05818d23a9cecf2b7eb9075cc729
proforex2017 <sahbiachouris@gmail.com>
2025-03-14 09:25:41 +0100

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,7 @@
+// Get Notepad from global scope
+const Notepad = window.Notepad;
+
+// Render the app
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Notepad />);
+
